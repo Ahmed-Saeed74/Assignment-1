143d36abd95930fecd7fc58343d6cf5abefcfceb
Ahmed Saeed <maxman201171@gmail.com>
2021-04-18 00:12:07 +0200

    Assignment 1

--- main.c
+++ main.c
@@ -13,30 +13,39 @@ int year;
 } ;
 int main()
 {
-    printf("Welcome\n");
+    printf(" \n\n\n\n\n\t\t\t\t\t          *  *  *  *  *  *  *  *");
+		printf("\n\n\t\t\t\t\t          *                    *");
+		printf("\n\n\t\t\t\t\t          *       Welcome      *");
+		printf("\n\n\t\t\t\t\t          *                    *");
+		printf("\n\n\t\t\t\t\t          *  *  *  *  *  *  *  *");
+		printf("\n\n\n\n\n\t\t\t\t\t      Press any key to continue...... ");
+		getch();
+		system("cls");
     int i,j,n;
     struct student s[50];
     {
-        printf("How many students you want to input: ");
+        printf("\t\t\t\t\tHow many students you want to input: ");
     scanf("%d", &n);
     for (i = 0; i < n; ++i) {
-        printf("\nEnter your id: ");
+        printf("\n\t\t\t\t\tEnter your id: ");
         scanf("%d", &s[i].id);
-        printf("Enter first name: ");
+        printf("\t\t\t\t\tEnter first name: ");
         scanf("%s", s[i].firstName);
-        printf("Enter your Date of Birth [dd mm yyyy]: ");
+        printf("\t\t\t\t\tEnter your Date of Birth [dd mm yyyy]: ");
         scanf("%d%d%d",&s[i].dob.day,&s[i].dob.month,&s[i].dob.year);
-        printf("Enter the score of the last year: ");
+        printf("\t\t\t\t\tEnter the score of the last year: ");
         scanf("%d", &s[i].score);
     }
-    printf("\nDisplaying Information:\n\n");
+    system("cls");
+    printf("\n\t\t\t\t\tDisplaying Information:\n\n");
     for (j = 0; j < n; ++j) {
-        printf("\nid: %d\n", s[j].id);
-        printf("First name: ");
+        printf("\n\t\t\t\t\tStudent %d:\n",j+1);
+        printf("\n\t\t\t\t\tid: %d\n", s[j].id);
+        printf("\t\t\t\t\tFirst name: ");
         puts(s[j].firstName);
-        printf("Date of Birth[dd/mm/yyyy] : %d/%d/%d\n",s[j].dob.day,s[j].dob.month,s[j].dob.year );
-        printf("score: %d", s[j].score);
-        printf("\n");
+        printf("\t\t\t\t\tDate of Birth[dd/mm/yyyy] : %d/%d/%d\n",s[j].dob.day,s[j].dob.month,s[j].dob.year );
+        printf("\t\t\t\t\tscore: %d", s[j].score);
+        printf("\n\n\n");
     }
     };
 
